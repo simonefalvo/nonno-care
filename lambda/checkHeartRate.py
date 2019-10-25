8ae7ec1335532f9391a946fb1df0020f677f3ae3
smvfal <smvfal@gmail.com>
2019-10-25 11:34:04 +0200

    Dynamo GET delle informazioni dell'utente e controllo heartrate

--- lambda/checkHeartRate.py
+++ lambda/checkHeartRate.py
@@ -12,11 +12,22 @@ def handler(event, context):
         timestamp = attributes["timestamp"]["Value"]
         latitude = attributes["latitude"]["Value"]
         longitude = attributes["longitude"]["Value"]
-        heart_rate = attributes["heart_rate"]["Value"]
+        heart_rate = int(attributes["heart_rate"]["Value"])
 
         # Get the service resource.
         dynamodb = boto3.resource('dynamodb')
-
         table = dynamodb.Table('nonno-stack-UserDataTable-RMJMKMKYK11F')
 
-        print("UserDataTable creation time:", table.creation_date_time)
+        response = table.get_item(
+            Key={
+                'sensor_id': sensor_id
+            }
+        )
+        item = response['Item']
+        print("Item:\n", item)
+
+        avg_hrate = int(item['avg_hrate'])
+        var_hrate = int(item['var_hrate'])
+
+        if not avg_hrate - var_hrate <= heart_rate <= avg_hrate + var_hrate:
+            print("WARNING: Abnormal Heartbeats")
