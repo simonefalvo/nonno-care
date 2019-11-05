4f99ead5131fe19554d849d5f31df081a1379d0b
smvfal <smvfal@gmail.com>
2019-11-05 15:41:25 +0100

    refactoring

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
-aws cloudformation package --template-file template.yaml \
-    --output-template packaged.yaml --s3-bucket nonno-care
-aws cloudformation deploy --template-file packaged.yaml --region eu-west-3 \
-    --capabilities CAPABILITY_IAM --stack-name nonno-stack
+aws cloudformation package \
+    --template-file template.yaml \
+    --output-template packaged.yaml \
+    --s3-bucket nonno-care
+
+aws cloudformation deploy \
+    --template-file packaged.yaml \
+    --region eu-west-3 \
+    --capabilities CAPABILITY_IAM \
+    --stack-name nonno-stack
--- describe_stack.sh
+++ describe_stack.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
-aws cloudformation describe-stacks --stack-name nonno-stack --region eu-west-3 \
+aws cloudformation describe-stacks \
+    --stack-name nonno-stack \
+    --region eu-west-3 \
     --query "Stacks[].Outputs"
