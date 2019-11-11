230f945e7f872e51e45d075805ad6bc3f28517f4
Aldo Pietrangeli <pietrangeli.aldo@gmail.com>
2019-11-11 11:30:51 +0100

    fix fall_data optional

--- lambda/eventProcessor.py
+++ lambda/eventProcessor.py
@@ -20,25 +20,23 @@ def handler(event, context):
         latitude = attributes["latitude"]["stringValue"]
         longitude = attributes["longitude"]["stringValue"]
         heart_rate = attributes["heart_rate"]["stringValue"]
-        fall_data = attributes["fall"]["stringValue"]
 
         # Get the service resource.
         dynamodb = boto3.resource('dynamodb')
 
         table = dynamodb.Table(os.environ['SENSOR_DATA_TABLE'])
-
-        table.put_item(
-            Item={
+        item ={
                 'sensor_id': sensor_id,
                 'timestamp': timestamp,
                 'latitude': latitude,
                 'longitude': longitude,
-                'heart_rate': heart_rate,
-                'fall': fall_data
+                'heart_rate': heart_rate
             }
-        )
 
-        if len(fall_data) > 0:
+        if 'fall' in attributes:
+            fall_data = attributes["fall"]["stringValue"]
+            item['fall'] = fall_data
+
             message = "Possibile caduta"
             topic = os.environ['SNS_TOPIC_FALL_DETECTION']
             print(publish_topic(topic, message, sensor_id, timestamp, latitude, longitude, heart_rate, fall_data))
@@ -48,41 +46,47 @@ def handler(event, context):
             topic = os.environ['SNS_TOPIC_HEARTRATE']
             print(publish_topic(topic, message, sensor_id, timestamp, latitude, longitude, heart_rate))
 
+        table.put_item(Item=item)
+
 
 # TODO: creare un layer
-def publish_topic(topic, message, sensor_id, timestamp, latitude, longitude, heart_rate, fall_data="ND"):
+def publish_topic(topic, message, sensor_id, timestamp, latitude, longitude, heart_rate, fall_data=None):
+
     print("Topic dentro la funzione:", topic)
     # Create an SNS client
     sns = boto3.client('sns')
+
+    attributes = {
+        'sensor_id': {
+            'DataType': 'Number',
+            'StringValue': sensor_id
+        },
+        'timestamp': {
+            'DataType': 'String',
+            'StringValue': timestamp
+        },
+        'latitude': {
+            'DataType': 'Number.float',
+            'StringValue': latitude
+        },
+        'longitude': {
+            'DataType': 'Number.float',
+            'StringValue': longitude
+        },
+        'heart_rate': {
+            'DataType': 'Number',
+            'StringValue': heart_rate
+        }
+    }
+    if fall_data is not None:
+        attributes['fall'] = {
+                'DataType': 'String',
+                'StringValue': fall_data
+            }
 
     # Publish a simple message to the specified SNS topic
     return sns.publish(
         TopicArn=topic,
         Message=message,
-        MessageAttributes={
-            'sensor_id': {
-                'DataType': 'Number',
-                'StringValue': sensor_id
-            },
-            'timestamp': {
-                'DataType': 'String',
-                'StringValue': timestamp
-            },
-            'latitude': {
-                'DataType': 'Number.float',
-                'StringValue': latitude
-            },
-            'longitude': {
-                'DataType': 'Number.float',
-                'StringValue': longitude
-            },
-            'heart_rate': {
-                'DataType': 'Number',
-                'StringValue': heart_rate
-            },
-            'fall_data': {
-                'DataType': 'String',
-                'StringValue': fall_data
-            }
-        }
+        MessageAttributes=attributes
     )
