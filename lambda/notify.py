ae34027b2c5f8cf6c14c4d446ef517979e1f5581
smvfal <smvfal@gmail.com>
2019-12-08 21:37:04 +0100

    aggiunte coordinate geografiche nel testo dell'email

--- lambda/notify.py
+++ lambda/notify.py
@@ -33,7 +33,9 @@ def handler(event, context):
         # send email to subscribers
         email = get_subscribers(dynamodb, sensor_id)
         #print("Email: ", email)
-        send_email(ses, email, message)
+        position = "Latitudine:{}, Longitudine:{}"\
+            .format(latitude, longitude)
+        send_email(ses, email, message, position)
 
         print("JOB_ID {}, RequestId: {}, BatchSize: {}"
               .format(sensor_id + timestamp.replace('.', '-'), context.aws_request_id, batch_size))
@@ -50,25 +52,22 @@ def get_subscribers(dynamodb, sensor_id):
     return response['Item']['email']
 
 
-def send_email(ses_client, recipient, message):
+def send_email(ses_client, recipient, message, position):
     sender = "Nonno Care <nonnocare.notify@gmail.com>"
     subject = "nonno ALERT"
 
     # The email body for recipients with non-HTML email clients.
-    body_text = message
+    body_text = message + ", " + position
 
     # The HTML body of the email.
     body_html = """<html>
     <head></head>
     <body>
       <h1>{}</h1>
-      <p>This email was sent with
-        <a href='https://aws.amazon.com/ses/'>Amazon SES</a> using the
-        <a href='https://aws.amazon.com/sdk-for-python/'>
-          AWS SDK for Python (Boto)</a>.</p>
+      <p>{}</p>
     </body>
     </html>
-                """.format(message)
+                """.format(message, position)
 
     # The character encoding for the email.
     charset = "UTF-8"
